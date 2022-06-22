cda22e798b50d8ee150c6b1f28ee8c5ac8e05198
Gilberto Taccari <gilberto.taccari@gmail.com>
2022-06-22 20:03:23 +0200

    Adjust sources indentation

--- src/main/java/it/tccr/puppet/PuppetApplication.java
+++ src/main/java/it/tccr/puppet/PuppetApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PuppetApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PuppetApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(PuppetApplication.class, args);
+  }
 
 }
