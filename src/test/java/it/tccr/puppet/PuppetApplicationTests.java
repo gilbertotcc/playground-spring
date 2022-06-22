3b0f20c7484b91822cebc11b824d2be83668e632
Gilberto Taccari <gilberto.taccari@gmail.com>
2022-06-22 19:59:06 +0200

    Add Spring Boot service scaffold
    Intent of this commit is to setup the minimum project to start playing
    with Spring Boot.

---
+++ src/test/java/it/tccr/puppet/PuppetApplicationTests.java
@@ -0,0 +1,13 @@
+package it.tccr.puppet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
