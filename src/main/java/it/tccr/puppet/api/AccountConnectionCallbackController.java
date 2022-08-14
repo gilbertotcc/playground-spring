28af5537628460265180dd2a0403958bcc08afed
Gilberto Taccari <gilberto.taccari@gmail.com>
2022-08-14 18:50:58 +0200

    Implement callback endpoint

--- src/main/java/it/tccr/puppet/api/AccountConnectionCallbackController.java
+++ src/main/java/it/tccr/puppet/api/AccountConnectionCallbackController.java
@@ -1,16 +1,28 @@
 package it.tccr.puppet.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
 
 import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.MOVED_PERMANENTLY;
 
 @RestController
 @RequiredArgsConstructor
 public class AccountConnectionCallbackController implements AccountConnectionCallbackApi {
 
   private final NativeWebRequest request;
+
+  @Override
+  public ResponseEntity<Void> notifyAggregationCompleted(String code, String credentialsId) {
+    return ResponseEntity
+      .status(MOVED_PERMANENTLY)
+      .header(HttpHeaders.LOCATION, "https://console.tink.com/callback")
+      .build();
+  }
 
   @Override
   public Optional<NativeWebRequest> getRequest() {
